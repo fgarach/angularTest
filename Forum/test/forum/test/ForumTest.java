388b2985a066b5141fc0600d01f03ec360f0f033
RDSopra <"romain.dao01@gmail.com>
2016-02-25 14:48:21 +0100

    mise en place fichier test Romain

---
+++ Forum/test/forum/test/ForumTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forum.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author admin
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:/C:\\Users\\admin\\Documents\\NetBeansProjects\\forum\\Forum\\application-context.xml")
+public class ForumTest {
+    
+
+}
